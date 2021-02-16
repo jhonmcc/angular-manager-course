e7f03e0490e07228d42ed03bc28fcd0b0115b578
jhonmcc <jhonmcc@gmail.com>
2021-02-15 22:00:12 -0300

    criado projeto inicial, adicionado bootstrap, criado componente course-list, criado model course

---
+++ src/app/courses/models/course.ts
@@ -0,0 +1,9 @@
+export class Course {
+    id: number | undefined;
+    name: string | undefined;
+    imgUrl: string | undefined;
+    price: number | undefined;
+    code: string | undefined;
+    duration: number | undefined;
+    rating: number | undefined;
+}
