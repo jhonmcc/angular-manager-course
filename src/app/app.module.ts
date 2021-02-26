b8d84c968059c0e9bfcd1f15b4da4ac3e4479358
dev <dev@DESKTOP-2UE8KHN.portoseguro.brasil>
2021-02-25 23:48:13 -0300

    adicionado two-way databinding via set e get, adicionado filtro pelo campo input, adicionado FormsModule para o twoway-databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list/course-list.component';
 import { StarComponent } from './star/star.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { StarComponent } from './star/star.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/courses/course-list/course-list.component.ts
+++ src/app/courses/course-list/course-list.component.ts
@@ -9,12 +9,25 @@ import { CourseService } from '../course.service';
   styleUrls: ['./course-list.component.scss']
 })
 export class CourseListComponent implements OnInit {
-  courses: Course[] = [] // inicializando objeto tipado de models
-  
+  _courses: Course[] = [] // inicializando objeto tipado de models
+  _filterBy: string = ''
+
+  filteredCourses: Course[] = []
+
   constructor(private courseService: CourseService) { }
 
   ngOnInit(): void {
-    this.courses = this.courseService.retrieveAll()
+    this._courses = this.courseService.retrieveAll()
+    this.filteredCourses = this._courses
   }
 
+  set filter(value: string){
+    this._filterBy = value
+
+    this.filteredCourses = this._courses.filter((course: Course) => course.name.toLowerCase().indexOf(this._filterBy.toLowerCase()) > -1)
+  }
+
+  get filter(){
+    return this._filterBy
+  }
 }
